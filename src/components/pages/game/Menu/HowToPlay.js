9cb88772769aa071696330e436e765acc2492d4c
Rafa Sadiq <school.rs.83@gmail.com>
2022-04-01 15:17:10 -0500

    up to date

--- src/components/pages/game/Menu/HowToPlay.js
+++ src/components/pages/game/Menu/HowToPlay.js
@@ -11,7 +11,7 @@ function HowToPlay () {
                 </h2>
                 <div className="spacer-10"></div>   
                 <p className="para">
-                    The object of the game is to clear all of the numbers 1 - 10 from the red number line.
+                    The object of the game is to clear all of the numbers 1 - 9 from the red number line.
                     Single Player Game
                 </p>
                 <div className="spacer-10"></div> 
@@ -20,9 +20,10 @@ function HowToPlay () {
                     <li>Select a number or multiple numbers from the red number line that add up to the sum of the dots on the displayed dice.</li>
                     <li>Roll again and repeat until the number line is clear.</li>
                     <li>If you are unable to match the remaining numbers in the red number line to the dice, continue rolling until you have a playable match.</li>
+                    <li>Game Ends when you pick the last number and roll the dice.</li>
                 </ul>
                 <div className="spacer-10"></div>   
-                <h2>
+                {/* <h2>
                     Two Player Game
                 </h2>
                 <div className="spacer-10"></div> 
@@ -32,7 +33,7 @@ function HowToPlay () {
                     <li>To complete the turn, click the "Play Selected Numbers" button.</li>
                     <li>If a player is unable to match any of the remaining numbers in their red number line to the sum of the dice, that player ends their turn by clicking the "Play Selected Numbers" button.</li>
                     <li>The game ends when one player has cleared all of the numbers from their number line.</li>
-                </ul>
+                </ul> */}
             </div>
         </div>
     )
